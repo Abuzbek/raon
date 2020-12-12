c9145b94d5a76d71510e9754f05ddc926eda4ba6
AbuzCoder <abuzcoder@gmail.com>
2020-12-12 07:27:12 +0500

    add nuxt-project

---
+++ layouts/default.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      color="white"
+      dense
+      fixed
+      height="64px"
+      style="z-index:10;"
+    >
+    <nuxt-link exact no-prefetch to="/">
+      <v-img
+        alt="star Logo"
+        class="shrink ml-12"
+        contain
+        :src="require('../assets/images/logo.png')"
+        transition="scale-transition"
+        height="64px"
+      />
+    </nuxt-link>
+      <v-spacer></v-spacer>
+      <v-menu
+        rounded="b-xl"
+        offset-y
+        v-for="(n, i) in menus"
+        :key="i"
+      >
+      <template v-slot:activator="{ attrs, on }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          text
+          class="btn_text"
+          active-class="btn_text_active"
+        >
+          {{ n.title }}
+          <v-icon right v-if="n.item">
+            mdi-chevron-down
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <router-link class="itemHref" to="#!"
+        v-for="(n, i) in n.item"
+          :key="i">
+        <v-list-item
+          link
+        >
+          <v-list-item-title>
+            {{n}}
+          </v-list-item-title>
+          
+        </v-list-item>
+        </router-link>
+      </v-list>
+    </v-menu>
+    </v-app-bar>
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menus:[
+        {
+          title:'Главная',
+          item:[
+            'Слоган бренда',
+            'Слоган бренда'
+          ]
+        },
+        {
+          title: 'O нас'
+        },
+        {
+          title:'Женщины',
+          item:[
+            'Декоративная Косметика',
+            'Средства для депиляции',
+            'Уход за волосами',
+            'Пакет масок',
+            'Полноценный уход за кожей'
+          ]
+        },
+        {
+          title:'Мужчины',
+          item:[
+            'Декоративная Косметика',
+            'Средства для депиляции',
+            'Уход за волосами',
+            'Пакет масок',
+            'Полноценный уход за кожей',
+
+          ]
+        },
+        {
+          title:'Aккаунт'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.btn_text{
+  transition:.3s;
+  &:hover{
+    color:#ffae33;
+  }
+  &_active{
+    color:#ffae33;
+  }
+}
+.itemHref{
+  color: black;
+  text-decoration: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,132 @@
+<template>
+<div> 
+  <header>
+     <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide
+        v-for="n in banners"
+        :key="n"
+        :style="`background : url(${require(`../assets/images/${n}`)})no-repeat center center / cover;`"
+        class="swiperSlide"
+      >
+      </swiper-slide>
+    </swiper>
+    <div class="swiper-pagination swiperPag" slot="pagination"></div>
+  </header>
+  <v-container>
+    <h1 class="text-center">BEST SELLER</h1>
+     <v-row>
+    <v-col v-for="n in 12" :key="n" cols="6" xl="2" lg="3" md="4">
+      <v-card
+        class="mx-auto"
+      >
+        <v-img
+          v-ripple="{class: 'warning--text'}"
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
+        <v-card-subtitle class="pb-0">
+          Number 10
+        </v-card-subtitle>
+
+        <v-card-text class="text--primary pb-2">
+          <div>Whitehaven Beach</div>
+          <v-row>
+            <v-col v-if="n.sale" cols="12" md="6">
+              {{n.sale}}
+            </v-col>
+            <v-col class="pt-1 pb-1" cols="12" md="6">
+              193,000  
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+            color="warning"
+            text
+          >
+            Share
+          </v-btn>
+
+          <v-btn
+            color="warning"
+            text
+          >
+            Explore
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  </v-container>
+ 
+</div>
+</template>
+
+<script>
+import SwiperCore , {Pagination, EffectFade} from 'swiper'
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Pagination, EffectFade]);
+export default {
+  data() {
+    return {
+      banners: [ '1.jpg', '2.jpg', '3.jpg' ],
+      swiperOptions: {
+        loop:true,
+        effect: 'fade',
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        // Some Swiper option/callback...
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  }
+}
+</script>
+
+<style lang="scss">
+.swiperSlide{
+  width: 100%;
+  height: 600px;
+}
+header{
+  position: relative;
+  padding-bottom: 60px;
+  .swiperPag{
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    .swiper-pagination-bullet{
+      margin: 0px 5px;
+      width: 10px;
+      height: 10px;
+      background-color: transparent;
+      border: 2px solid #a5a5a5;
+      border-radius: 6px;
+      outline: 1px solid transparent;
+      cursor: pointer;
+      -webkit-transition: .3s ease-in-out;
+      transition: .3s ease-in-out;
+      opacity:1 !important;
+      &-active{
+        border: 2px solid #000;
+        cursor: pointer;
+        -webkit-transition: .3s ease-in-out;
+        transition: .3s ease-in-out;
+      }
+      
+    }
+  }
+}
+</style>
